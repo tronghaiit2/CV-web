99416e8f86251c8a7ed0b390583f452ce9f64645
tronghaiit2 <53091357+tronghaiit2@users.noreply.github.com>
2020-07-26 18:35:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var circle = document.getElementById("circle");
+var name = document.getElementById("name");  var next = document.getElementById("next");
+var back = document.getElementById("back");
+
+var rotateValue = "rotate(0deg)";
+var rotateSum;
+
+    next.onclick = function()
+    {
+        rotateSum = rotateValue + "rotate(-60deg)";
+        circle.style.transform = rotateSum;
+        rotateValue = rotateSum;
+    }
+
+    back.onclick = function()
+    {
+        rotateSum = rotateValue + "rotate(+60deg)";
+        circle.style.transform = rotateSum;
+        rotateValue = rotateSum;
+    }
+
