f86cb970efe6db89d77d212da19ae558569deec7
tronghaiit2 <53091357+tronghaiit2@users.noreply.github.com>
2020-08-03 21:02:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var show = document.getElementById("nav-links");
-        function showMenu() {
-            show.style.right ="0";
+    function showMenu() {
+        show.style.right ="0";
+    }
+    function closeMenu() {
+        show.style.right ="-120px";
+    }
+var avatar = document.getElementById("avatar");
+var profile = document.getElementById("profile");
+    profile.onclick = function (){
+        if (profile.style.display = "block") {
+            profile.style.display = "none";
         }
-        function closeMenu() {
-            show.style.right ="-120px";
+    }
+    avatar.onclick = function (){
+        if (profile.style.display = "none") {
+            profile.style.display = "block";
         }
-
+    }
+    
 
 var circle = document.getElementById("circle");
 var name = document.getElementById("name");  var next = document.getElementById("next");
@@ -17,14 +29,14 @@ var rotateSum;
     next.onclick = function()
     {
         rotateSum = rotateValue + "rotate(-60deg)";
-        home.style.transform = rotateSum;
+        main.style.transform = rotateSum;
         rotateValue = rotateSum;
     }
 
     back.onclick = function()
     {
         rotateSum = rotateValue + "rotate(+60deg)";
-        home.style.transform = rotateSum;
+        main.style.transform = rotateSum;
         rotateValue = rotateSum;
     }
 
